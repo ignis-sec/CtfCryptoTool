e0c167aa8319c01828d3bfebc214373f080f7ae7
Ata <ahakcil@gmail.com>
2020-02-27 17:30:19 +0200

    Added swap delimiter module

---
+++ crypto/delimiter.py
@@ -0,0 +1,26 @@
+
+import re
+
+name="swapDelimiter"
+
+# Convert delimiter to standard, whitespace
+
+
+def check(result,text,**kwargs):
+    if("cipherLength" in result):
+        if(len(re.findall(",",text))>=result["cipherLength"]/5 and len(re.findall(",",text))<=result["cipherLength"]/1.9):
+            return True
+        if(len(re.findall(", ",text))>=result["cipherLength"]/5 and len(re.findall(",",text))<=result["cipherLength"]/1.9):
+            return True
+        if(len(re.findall("\t",text))>=result["cipherLength"]/5 and len(re.findall(",",text))<=result["cipherLength"]/1.9):
+            return True
+        if(len(re.findall("\\\\",text))>=result["cipherLength"]/5 and len(re.findall(",",text))<=result["cipherLength"]/1.9):
+            return True
+        if(len(re.findall("/",text))>=result["cipherLength"]/5 and len(re.findall(",",text))<=result["cipherLength"]/1.9):
+                return True
+    return False
+
+
+def decrypt(text, **kwargs):
+    text = re.sub('(,)|(, )|(\t)|(\\\\)|(/)', ' ', text)
+    return text
